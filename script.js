318e806b02fe634a905cbe7b6635493a2f5de60f
Thawane <thawaneapoliveira@gmail.com>
2022-08-03 16:10:15 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -5,11 +5,26 @@ const button = document.querySelector('#criar-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 
 function newListElement() {
-    const newTask = document.createElement('li');
-    newTask.innerHTML = input.value;
-    ol.appendChild(newTask);
-    input.value = ''; 
-} 
+  const newTask = document.createElement('li');
+  newTask.innerHTML = input.value;
+  newTask.classList.add('new-task');
+  ol.appendChild(newTask);
+  newTask.addEventListener('click', changeBGColor);
+  input.value = '';
+}
 
 button.addEventListener('click', newListElement);
+
+// 7
+
+const selectedTask = document.getElementsByClassName('new-task')
+
+function changeBGColor(event) {
+  console.log('tarefa nova', event.target);
+  for (let index = 0; index < selectedTask.length; index += 1) {
+    selectedTask[index].classList.remove('new-task');
+  }
+  event.target.classList.add('selected');
+  event.target.style.backgroundColor = 'gray';
+}
 
